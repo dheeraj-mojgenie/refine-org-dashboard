afa89b4c061b01a2a62b8edce21a225143e91e27
dheeraj <dheeraj@mojgenie.com>
2025-12-15 15:03:52 +0530

    Initial commit from create-refine-app

---
+++ src/App.tsx
@@ -0,0 +1,143 @@
+import {
+  AuthProvider,
+  GitHubBanner,
+  Refine,
+  WelcomePage,
+} from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import { useKeycloak } from "@react-keycloak/web";
+import dataProvider, { GraphQLClient } from "@refinedev/graphql";
+import routerProvider, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router";
+import axios from "axios";
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./App.css";
+import { Toaster } from "./components/refine-ui/notification/toaster";
+import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
+import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
+
+const API_URL = "https://your-graphql-url/graphql";
+
+const client = new GraphQLClient(API_URL);
+const gqlDataProvider = dataProvider(client);
+
+function App() {
+  const { keycloak, initialized } = useKeycloak();
+
+  if (!initialized) {
+    return <div>Loading...</div>;
+  }
+
+  const authProvider: AuthProvider = {
+    login: async () => {
+      const urlSearchParams = new URLSearchParams(window.location.search);
+      const { to } = Object.fromEntries(urlSearchParams.entries());
+      await keycloak.login({
+        redirectUri: to ? `${window.location.origin}${to}` : undefined,
+      });
+      return {
+        success: true,
+      };
+    },
+    logout: async () => {
+      try {
+        await keycloak.logout({
+          redirectUri: window.location.origin,
+        });
+        return {
+          success: true,
+          redirectTo: "/login",
+        };
+      } catch (error) {
+        return {
+          success: false,
+          error: new Error("Logout failed"),
+        };
+      }
+    },
+    onError: async (error) => {
+      console.error(error);
+      return { error };
+    },
+    check: async () => {
+      try {
+        const { token } = keycloak;
+        if (token) {
+          axios.defaults.headers.common = {
+            Authorization: `Bearer ${token}`,
+          };
+          return {
+            authenticated: true,
+          };
+        } else {
+          return {
+            authenticated: false,
+            logout: true,
+            redirectTo: "/login",
+            error: {
+              message: "Check failed",
+              name: "Token not found",
+            },
+          };
+        }
+      } catch (error) {
+        return {
+          authenticated: false,
+          logout: true,
+          redirectTo: "/login",
+          error: {
+            message: "Check failed",
+            name: "Token not found",
+          },
+        };
+      }
+    },
+    getPermissions: async () => null,
+    getIdentity: async () => {
+      if (keycloak?.tokenParsed) {
+        return {
+          name: keycloak.tokenParsed.family_name,
+        };
+      }
+      return null;
+    },
+  };
+
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <ThemeProvider>
+          <DevtoolsProvider>
+            <Refine
+              dataProvider={gqlDataProvider}
+              notificationProvider={useNotificationProvider()}
+              authProvider={authProvider}
+              routerProvider={routerProvider}
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+                projectId: "qyyoRh-HW3UAy-biytvo",
+              }}
+            >
+              <Routes>
+                <Route index element={<WelcomePage />} />
+              </Routes>
+              <Toaster />
+              <RefineKbar />
+              <UnsavedChangesNotifier />
+              <DocumentTitleHandler />
+            </Refine>
+            <DevtoolsPanel />
+          </DevtoolsProvider>
+        </ThemeProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
